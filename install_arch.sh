bee3cd57056ecade3254d745b2ed0dd1a714305b
tarekadel2020 <78466267+tarekadel2020@users.noreply.github.com>
2021-02-07 15:50:22 +0200

    Update install_arch.sh

--- install_arch.sh
+++ install_arch.sh
@@ -23,7 +23,7 @@ wifi_pass=""
 
 read -p "Are you install base ? [Y-N]" accept_base
 
-if [ $accept_base == "y"] || [ $accept_base == "Y"] ; then
+if [ $accept_base="y" ] || [ $accept_base="Y"] ; then
 
 		# ask install base or install desktop #
 
@@ -152,101 +152,102 @@ fi
 
 
 read -p "Are you install gui ? [Y-N]" accept_gui
-if [ $accept_gui == "y"] || [ $accept_gui == "Y"] ; then
+if [ $accept_gui="y" ] || [ $accept_gui="Y" ] ; then
 
-##  Desktop Environment ##
+		##  Desktop Environment ##
 
-		## (gnome - kde - xfce - mate - cinnamon - lxde - i3-wm - i3-gaps - dwm - deepin)
+		## (gnome - kde - xfce - mate - cinnamon - lxde - i3-wm - i3-gaps - dwm - deepin) ##
 
 		case $(echo "$Desktop_GUI" |tr [:upper:] [:lower:]) in
 		
-			"gnome" )
+			"gnome")
 
-			arch-chroot /mnt pacman -Syu --noconfirm --needed xorg xorg-server
-			arch-chroot /mnt pacman -Syu --noconfirm --needed gnome gdm
-			sleep 3
-			arch-chroot /mnt systemctl start gdm.service
-			sleep 3
-			arch-chroot /mnt systemctl enable gdm.service
-			sleep 3
-			arch-chroot /mnt systemctl enable NetworkManager.service
-
-			;;
+				arch-chroot /mnt pacman -Syu --noconfirm --needed xorg xorg-server
+				arch-chroot /mnt pacman -Syu --noconfirm --needed gnome gdm
+				sleep 3
+				arch-chroot /mnt systemctl start gdm.service
+				sleep 3
+				arch-chroot /mnt systemctl enable gdm.service
+				sleep 3
+				arch-chroot /mnt systemctl enable NetworkManager.service
+				;;
 			"kde" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed xorg plasma plasma-meta plasma-wayland-session kde-applications-meta sddm
-			arch-chroot /mnt systemctl enable sddm.service
-			arch-chroot /mnt systemctl enable NetworkManager.service
-
-			;;
+				arch-chroot /mnt pacman -Syu --noconfirm --needed xorg plasma plasma-meta plasma-wayland-session kde-applications-meta sddm
+				arch-chroot /mnt systemctl enable sddm.service
+				arch-chroot /mnt systemctl enable NetworkManager.service
+				;;
 			"xfce" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed xfce4 xfce4-goodies lightdm lightdm-gtk-greeter xorg-server
-			arch-chroot /mnt systemctl enable lightdm.service
-			arch-chroot /mnt systemctl enable NetworkManager.service
+				arch-chroot /mnt pacman -Syu --noconfirm --needed xfce4 xfce4-goodies lightdm lightdm-gtk-greeter xorg-server
+				arch-chroot /mnt systemctl enable lightdm.service
+				arch-chroot /mnt systemctl enable NetworkManager.service
 
-			## xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk ##
-
-
-			;;
+				## xfce4 mousepad parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk ##
+				
+				;;
 			"mate" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed mate mate-extra lightdm lightdm-gtk-greeter xorg-server
-			arch-chroot /mnt systemctl enable lightdm.service
-			arch-chroot /mnt systemctl enable NetworkManager.service
-
-			;;
+				arch-chroot /mnt pacman -Syu --noconfirm --needed mate mate-extra lightdm lightdm-gtk-greeter xorg-server
+				arch-chroot /mnt systemctl enable lightdm.service
+				arch-chroot /mnt systemctl enable NetworkManager.service
+				;;
+			
 			"cinnamon" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed cinnamon lightdm lightdm-gtk-greeter xorg-server
-			arch-chroot /mnt systemctl enable lightdm.service
-			arch-chroot /mnt systemctl enable NetworkManager.service
-
-			;;
+				arch-chroot /mnt pacman -Syu --noconfirm --needed cinnamon lightdm lightdm-gtk-greeter xorg-server
+				arch-chroot /mnt systemctl enable lightdm.service
+				arch-chroot /mnt systemctl enable NetworkManager.service
+				;;
+				
 			"lxde" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed lxde lxdm
-			arch-chroot /mnt systemctl enable lxdm.service
+				arch-chroot /mnt pacman -Syu --noconfirm --needed lxde lxdm
+				arch-chroot /mnt systemctl enable lxdm.service
+				;;
 
-			;;
+
 			"i3-wm" )
 			arch-chroot /mnt pacman -Syu --noconfirm --needed i3-wm i3blocks i3lock i3status dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorg-server
 			arch-chroot /mnt systemctl enable lightdm.service
 
 			;;
 			"i3-gaps" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed i3-gaps i3blocks i3lock i3status dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorg-server
-			arch-chroot /mnt systemctl enable lightdm.service
+				arch-chroot /mnt pacman -Syu --noconfirm --needed i3-gaps i3blocks i3lock i3status dmenu rxvt-unicode lightdm lightdm-gtk-greeter xorg-server
+				arch-chroot /mnt systemctl enable lightdm.service
+				;;
+			
+			"dwm")
+				arch-chroot /mnt pacman -Syu --noconfirm --needed base-devel libx11 libxft xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
+				arch-chroot /mnt mkdir /home/$User_Name/.config
+				arch-chroot /mnt mkdir /home/$User_Name/.config/dwm
+				arch-chroot /mnt mkdir /home/$User_Name/.config/st
+				arch-chroot /mnt mkdir /home/$User_Name/.config/dmenu
 
-			;;
-			"dwm" )
-			arch-chroot /mnt pacman -Syu --noconfirm --needed base-devel libx11 libxft xorg-server xorg-xinit xorg-xrandr xorg-xsetroot
-			arch-chroot /mnt mkdir /home/$User_Name/.config
-			arch-chroot /mnt mkdir /home/$User_Name/.config/dwm
-			arch-chroot /mnt mkdir /home/$User_Name/.config/st
-			arch-chroot /mnt mkdir /home/$User_Name/.config/dmenu
+				arch-chroot /mnt git clone git://git.suckless.org/dwm /home/$User_Name/.config/dwm
+				arch-chroot /mnt git clone git://git.suckless.org/st /home/$User_Name/.config/st
+				arch-chroot /mnt git clone git://git.suckless.org/dmenu /home/$User_Name/.config/dmenu
+				arch-chroot /mnt cd /home/$User_Name/.config/dwm && make clean install
+				sleep 2
+				arch-chroot /mnt cd /home/$User_Name/.config/st && make clean install
+				sleep 2
+				arch-chroot /mnt cd /home/$User_Name/.config/dmenu && make clean install
+				sleep 2
 
-			arch-chroot /mnt git clone git://git.suckless.org/dwm /home/$User_Name/.config/dwm
-			arch-chroot /mnt git clone git://git.suckless.org/st /home/$User_Name/.config/st
-			arch-chroot /mnt git clone git://git.suckless.org/dmenu /home/$User_Name/.config/dmenu
-			arch-chroot /mnt cd /home/$User_Name/.config/dwm && make clean install
-			sleep 2
-			arch-chroot /mnt cd /home/$User_Name/.config/st && make clean install
-			sleep 2
-			arch-chroot /mnt cd /home/$User_Name/.config/dmenu && make clean install
-			sleep 2
-
-			arch-chroot /mnt pacman -Syu --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
-			sleep 3
-			arch-chroot /mnt systemctl enable lightdm.service
-			arch-chroot /mnt mkdir /usr/share/xsessions
-			arch-chroot /mnt echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Dwm\nComment=Dynamic window manager\nExec=dwm\nIcon=dwm\nType=XSession" > /usr/share/xsessions/dwm.desktop
-			arch-chroot /mnt echo -e "[Desktop]\nSession=dwm > /home/$User_Name/.dmrc
+				arch-chroot /mnt pacman -Syu --noconfirm --needed lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings
+				sleep 3
+				arch-chroot /mnt systemctl enable lightdm.service
+				arch-chroot /mnt mkdir /usr/share/xsessions
+				arch-chroot /mnt echo -e "[Desktop Entry]\nEncoding=UTF-8\nName=Dwm\nComment=Dynamic window manager\nExec=dwm\nIcon=dwm\nType=XSession" > /usr/share/xsessions/dwm.desktop
+				arch-chroot /mnt echo -e "[Desktop]\nSession=dwm" > /home/$User_Name/.dmrc
 
 
-			## APPS ##
-			## arch-chroot /mnt pacman -Syu --noconfirm --needed ttf-font-awesome alsa-utils firefox nitrogen htop ntfs-3g vlc sxhkd ##
-
-			;;
+				## APPS ##
+				## arch-chroot /mnt pacman -Syu --noconfirm --needed ttf-font-awesome alsa-utils firefox nitrogen htop ntfs-3g vlc sxhkd ##
+				
+				;;
+			
 			"deepin" )
-			;;
+				echo "deepin"
+				;;
 			
 		esac
+
 
 else
 	echo "your not install base Arch Gui"
