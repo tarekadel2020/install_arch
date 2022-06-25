b9932b6d88a10e353c71960db4237d8d8b601eb4
dev <78466267+tarekadel2020@users.noreply.github.com>
2022-06-25 15:39:10 +0200

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -110,7 +110,11 @@ Grub(){
 			arch-chroot /mnt pacman -Syu --noconfirm --needed  grub
 
 			arch-chroot /mnt grub-install --target=i386-pc $hard
-
+			
+			arch-chroot /mnt sed -i 's/#GRUB_DISABLE_OS_PROBER=false/GRUB_DISABLE_OS_PROBER=false' /etc/default/grub
+			
+			## echo "GRUB_DISABLE_OS_PROBER=false" >> /mnt/etc/default/grub
+			
 			arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 		fi
 		if [ $(echo "$Bios_Type" | tr [:upper:] [:lower:]) = "uefi" ]; then
