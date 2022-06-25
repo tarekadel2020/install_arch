9b381eb9433eebd836e8216c226e58c0cef48bf1
dev <78466267+tarekadel2020@users.noreply.github.com>
2022-06-25 15:49:16 +0200

    Update new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -191,7 +191,7 @@ XFCE(){
 	arch-chroot /mnt systemctl enable lightdm.service
 	arch-chroot /mnt systemctl enable NetworkManager.service
 	## APP
-        arch-chroot /mnt pacman -Syu --noconfirm --needed mousepad xfce4-screensaver xfce4-taskmanager xfce4-whiskermenu-plugin network-manager-applet epdfview
+        arch-chroot /mnt pacman -Syu --noconfirm --needed mousepad xfce4-screensaver xfce4-taskmanager xfce4-whiskermenu-plugin network-manager-applet epdfview alsa-utils pulseaudio
 	## parole ristretto thunar-archive-plugin thunar-media-tags-plugin xfce4-battery-plugin xfce4-datetime-plugin xfce4-mount-plugin xfce4-netload-plugin xfce4-notifyd xfce4-pulseaudio-plugin xfce4-screensaver xfce4-taskmanager xfce4-wavelan-plugin xfce4-weather-plugin xfce4-whiskermenu-plugin xfce4-xkb-plugin file-roller network-manager-applet leafpad epdfview galculator lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings capitaine-cursors arc-gtk-theme xdg-user-dirs-gtk ##
 
 }
